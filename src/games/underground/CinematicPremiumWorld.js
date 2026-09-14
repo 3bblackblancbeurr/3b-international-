e77f5bea8f411c23c551b6de89a2ab15854185b2
3bblackblancbeurr <3bblackblancbeurr@gmail.com>
2026-09-14 21:44:03 +0200

    feat: integrate premium world geometry v6

--- src/games/underground/CinematicPremiumWorld.js
+++ src/games/underground/CinematicPremiumWorld.js
@@ -3,6 +3,7 @@ import {CinematicDetailPass} from './CinematicDetailPass.js';
 import {HeroArchitecturePass} from './HeroArchitecturePass.js';
 import {WetWeatherMicroFX} from './WetWeatherMicroFX.js';
 import {SpeedAtmosphereFX} from './SpeedAtmosphereFX.js';
+import {WorldGeometryV6} from './WorldGeometryV6.js';
 
 export class CinematicPremiumWorld extends PremiumWorld{
   constructor(scene,curve,event,options={}){
@@ -10,6 +11,7 @@ export class CinematicPremiumWorld extends PremiumWorld{
     const shared={quality:options.quality||'high',palette:this.palette};
     this.cinematic=new CinematicDetailPass(scene,curve,event,shared);
     this.heroArchitecture=new HeroArchitecturePass(scene,curve,event,shared);
+    this.geometryV6=new WorldGeometryV6(scene,curve,event,shared);
     this.wetMicro=new WetWeatherMicroFX(scene,event,{quality:shared.quality});
     this.speedAtmosphere=new SpeedAtmosphereFX(scene,event,{quality:shared.quality,color:this.palette.matrix});
   }
@@ -17,8 +19,9 @@ export class CinematicPremiumWorld extends PremiumWorld{
     const base=super.update(args);
     const detail=this.cinematic.update({...args,wetness:base.wetness});
     const architecture=this.heroArchitecture.update(args);
+    const geometry=this.geometryV6.update(args);
     this.wetMicro.update({...args,wetness:base.wetness});
     this.speedAtmosphere.update({...args,wetness:base.wetness});
-    return {...base,...detail,...architecture};
+    return {...base,...detail,...architecture,...geometry};
   }
 }
