295852247c0d5a72ab9d80dba9163e4f6cd9f7d2
3bblackblancbeurr <3bblackblancbeurr@gmail.com>
2026-09-17 00:12:23 +0200

    Mobile: initialiser le runtime natif Capacitor

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import {LoyaltyProvider} from './loyalty/LoyaltyContext.jsx';
 import "./index.css";
+import { setupNativeApp } from './native/runtime.js';
+
+const nativeSetup = setupNativeApp().catch(() => () => {});
+if (import.meta.hot) import.meta.hot.dispose(() => { nativeSetup.then(dispose => dispose()); });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
