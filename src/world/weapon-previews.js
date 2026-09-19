84662cdc8c1f2c4afa26193a6b83fe8b4d93cd4b
3bblackblancbeurr <3bblackblancbeurr@gmail.com>
2026-09-19 16:54:42 +0200

    fix(world): use clearer spear and trident photo references

--- src/world/weapon-previews.js
+++ src/world/weapon-previews.js
@@ -3,7 +3,7 @@ const commonsPage=file=>`https://commons.wikimedia.org/wiki/File:${encodeURIComp
 
 const SOURCES={
   sabre:{file:'Sabre of Henryk Dembinski detail.jpg',credit:'Wikimedia Commons · CC0',position:'50% 48%'},
-  spear:{file:'Spear from Mandaya, Mindanao, Philippines, Honolulu Museum of Art.JPG',credit:'Hiart · Wikimedia Commons · CC0',position:'50% 45%'},
+  spear:{file:'Hand Spear (Nage-yari) or Javelin (Te-yari) with Sheath MET LC-36 25 1975a b-004.jpg',credit:'The Metropolitan Museum of Art · CC0',position:'50% 50%'},\n  trident:{file:'Trident, Burmese, 18th century.JPG',credit:'Wikimedia Commons · domaine public',position:'50% 48%'},
   bow:{file:'Recurve bow.jpg',credit:'Johnwxh30 · Wikimedia Commons · CC0',position:'50% 52%'},
   knives:{file:'Throwing knives.jpg',credit:'www.knifethrowing.info · CC BY-SA 3.0',position:'50% 50%'},
   gauntlet:{file:'Gauntlet for the Right Hand, Belonging to the Armor of Don Alonzo Pérez de Guzman el Bueno (1550–1619), Count of Niebla and Duke of Medina-Sidonia MET DP104361.jpg',credit:'The Metropolitan Museum of Art · CC0',position:'50% 52%'},
@@ -27,7 +27,7 @@ const MAP={
   tallinn:'sabre',
   bosphore:'knives',
   alger:'gauntlet',
-  carthage:'spear',
+  carthage:'trident',
   zellige:'shield',
   abanico:'fan',
   scissors:'scissors',
