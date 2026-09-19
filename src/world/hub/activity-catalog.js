a7a6a6c73664aa12f81bf0ec8b981e05ae7cf48c
3bblackblancbeurr <3bblackblancbeurr@gmail.com>
2026-09-19 16:25:42 +0200

    feat(world): add validated Hub event and secret catalogs

---
+++ src/world/hub/activity-catalog.js
@@ -0,0 +1,12 @@
+export const HUB_EVENT_IDS = [
+ 'market_night','heavy_rain_echo','train_breakdown','guardian_projection','dock_fog',
+ 'arena_public_challenge','power_flicker','memory_walk','city_showcase','workers_ceremony',
+];
+export const HUB_SECRET_IDS = [
+ 'secret_last_train','secret_waterfall_door','secret_three_lights','secret_rain_symbol',
+ 'secret_silent_cabin','secret_roof_signal','secret_abandoned_quay','secret_workers_names',
+ 'secret_lost_station','secret_broken_elevator','secret_arena_floor','secret_market_code',
+ 'secret_city_guest','secret_fog_tree','secret_archive_reverse','secret_train_window',
+];
+export const HUB_EVENT_SET=new Set(HUB_EVENT_IDS);
+export const HUB_SECRET_SET=new Set(HUB_SECRET_IDS);
