04a20662bc579f3fc65af914d3c0ee630be02e8f
3bblackblancbeurr <3bblackblancbeurr@gmail.com>
2026-09-21 15:25:02 +0200

    Rebase premium shell: src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import AppErrorBoundary from "./components/AppErrorBoundary.jsx";
 import {LoyaltyProvider} from './loyalty/LoyaltyContext.jsx';
 import "./index.css";
+import "./styles/platform-premium.css";
 import { setupNativeApp } from './native/runtime.js';
 
+document.documentElement.classList.add('js-app-ready');
 const nativeSetup = setupNativeApp().catch(() => () => {});
 if (import.meta.hot) import.meta.hot.dispose(() => { nativeSetup.then(dispose => dispose()); });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LoyaltyProvider><App /></LoyaltyProvider>
+    <AppErrorBoundary><LoyaltyProvider><App /></LoyaltyProvider></AppErrorBoundary>
   </React.StrictMode>
 );
