d1c5d10713454bcc0ccc3259e955ab35ad892bda
3bblackblancbeurr <3bblackblancbeurr@gmail.com>
2026-09-22 01:44:18 +0200

    France runtime v3: ajouter ThreeBWeatherDirector.h

---
+++ unreal/ThreeBWorld/Source/ThreeBWorld/ThreeBWeatherDirector.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ThreeBWeatherProfile.h"
+#include "ThreeBGameState.h"
+#include "ThreeBWeatherDirector.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(
+    FThreeBWeatherChanged,
+    EThreeBWeatherState, WeatherState,
+    float, TransitionSeconds
+);
+
+UCLASS(Blueprintable)
+class THREEBWORLD_API AThreeBWeatherDirector : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AThreeBWeatherDirector();
+
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="3B|Weather")
+    TObjectPtr<UThreeBWeatherProfile> WeatherProfile;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="3B|Weather", meta=(ClampMin="0.0"))
+    float DefaultTransitionSeconds = 8.0f;
+
+    UPROPERTY(BlueprintAssignable, Category="3B|Weather")
+    FThreeBWeatherChanged OnWeatherChanged;
+
+    UFUNCTION(BlueprintPure, Category="3B|Weather")
+    EThreeBWeatherState GetWeatherState() const { return CurrentWeather; }
+
+    UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category="3B|Weather")
+    bool SetWeatherState(EThreeBWeatherState NewState, float TransitionSeconds = -1.0f);
+
+protected:
+    virtual void BeginPlay() override;
+
+    UPROPERTY(ReplicatedUsing=OnRep_WeatherState, BlueprintReadOnly, Category="3B|Weather")
+    EThreeBWeatherState CurrentWeather = EThreeBWeatherState::LowCloud;
+
+    UPROPERTY(Replicated, BlueprintReadOnly, Category="3B|Weather")
+    float CurrentTransitionSeconds = 0.0f;
+
+    UFUNCTION()
+    void OnRep_WeatherState();
+
+    UFUNCTION()
+    void HandleStoryStateChanged(FThreeBReplicatedStoryState StoryState);
+
+    UFUNCTION(BlueprintImplementableEvent, Category="3B|Weather")
+    void ApplyWeatherPresentation(EThreeBWeatherState WeatherState, float TransitionSeconds);
+
+private:
+    void BroadcastWeatherPresentation();
+};
