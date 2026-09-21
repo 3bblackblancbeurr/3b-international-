8c843391e7db5e269c3b86013c4abe3f6a7863b7
3bblackblancbeurr <3bblackblancbeurr@gmail.com>
2026-09-21 16:25:27 +0200

    UE5.8 build: use current UBT defaults

--- unreal/ThreeBWorld/Source/ThreeBWorldEditor.Target.cs
+++ unreal/ThreeBWorld/Source/ThreeBWorldEditor.Target.cs
@@ -6,7 +6,7 @@ public class ThreeBWorldEditorTarget : TargetRules
     public ThreeBWorldEditorTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V6;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("ThreeBWorld");
     }
