404e967ecb0f19e5f2403d87441b7cfc29a222eb
3bblackblancbeurr <3bblackblancbeurr@gmail.com>
2026-09-25 06:43:44 +0200

    Expose enter and inspect actions for heritage facilities

--- src/world/interaction-system.js
+++ src/world/interaction-system.js
@@ -48,6 +48,7 @@ const TYPE_ACTIONS=Object.freeze({
  hubMission:['inspect'],
  hubTransport:['ride'],
  hubBuilding:['enter','inspect'],
+ hubHeritageFacility:['enter','inspect'],
  hubMilestone:['inspect'],
  hubEvent:['inspect'],
  hubSecret:['inspect'],
